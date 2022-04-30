80252b25d882b3b2dbe8d9e6df266a9f41bbd88b
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2022-04-30 08:51:03 -0300

    0.2

---
+++ 01.js
@@ -0,0 +1,8 @@
+const nome = prompt('what is your name?')
+
+const resultado = `Sair da conta ${nome}`
+
+const perfil = `${nome}`
+
+document.querySelector('#n').innerHTML = resultado
+document.querySelector('#conta').innerHTML = perfil
